bd9a1a3903ce30379ba777ba33d44599c3529bda
Erica Sanchez <sanchez.ericat@gmail.com>
2020-04-29 23:28:18 +0000

    fixed quit

--- calculator.py
+++ calculator.py
@@ -3,12 +3,28 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
-
+operator = [add, subtract, multiply, divide, square, cube,
+                        power, mod]
 # Replace this with your code
 while True:
     input_string = input("Enter your equation > ")
     tokens = input_string.split(' ')
-    if tokens == 'q':
+    if tokens[0] == 'q':
         break
-    elif tokens[0] == 'pow':
-        print(power(int(tokens[1]), int(tokens[2])))
+    else:
+        if tokens[0] == 'pow':
+            print(power(int(tokens[1]), int(tokens[2])))
+        elif tokens[0] == '+':
+            print(add(int(tokens[1]), int(tokens[2])))
+        elif tokens[0] == '-':
+            print(subtract(int(tokens[1]), int(tokens[2])))
+        elif tokens[0] == '*':
+            print(multiply(int(tokens[1]), int(tokens[2])))
+        elif tokens[0] == '/':   
+            print(divide(int(tokens[1]), int(tokens[2])))
+        elif tokens[0] == 'square':   
+            print(square(int(tokens[1])))
+        elif tokens[0] == 'cube':   
+            print(cube(int(tokens[1])))
+        elif tokens[0] == 'mod':   
+            print(mod(int(tokens[1]), int(tokens[2])))

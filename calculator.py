9b8972277991deba7300ec5626ee222d95258875
Erica Sanchez <sanchez.ericat@gmail.com>
2020-04-29 23:30:54 +0000

    deleted operator

--- calculator.py
+++ calculator.py
@@ -3,8 +3,7 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
-operator = [add, subtract, multiply, divide, square, cube,
-                        power, mod]
+
 # Replace this with your code
 while True:
     input_string = input("Enter your equation > ")

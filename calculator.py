1fd902c9eed78d7ab6f97bf2205f5cb9eea34552
Erica Sanchez <sanchez.ericat@gmail.com>
2020-04-29 22:56:10 +0000

    fixed pow to be recognized and added tokens as int

--- calculator.py
+++ calculator.py
@@ -5,3 +5,10 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+while True:
+    input_string = input("Enter your equation > ")
+    tokens = input_string.split(' ')
+    if tokens == 'q':
+        break
+    elif tokens[0] == 'pow':
+        print(power(int(tokens[1]), int(tokens[2])))
